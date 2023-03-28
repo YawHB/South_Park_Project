fecfdd52792efd55a55cfafaf713b9fdbb3560d4
YawHB <yawhboateng@gmail.com>
2023-03-28 21:05:36 +0200

    Added two additional functions
    Added two additional functions  for seperation of concerns

--- app.js
+++ app.js
@@ -2,14 +2,26 @@
 window.addEventListener("load", initApp);
 
 async function initApp() {
+  //1. Calls getData with an url argument
+  //2. Store the returned value in the array personList
+  //3. OBS: See the getData function to undrstand async and await
   const personList = await getData(
     "https://cederdorff.github.io/dat-js/05-data/southpark.json"
   );
 
+  // for (const character of personList) {
+  //   addCharacters(character)
+  // }
+  //Loops through each object in personList an sends the object to the 'addCharacters' function
   personList.forEach(addCharacters);
 }
 
+// We use await in order to wait for the response to complete. (we wait for the Proimise)
+//If the repsonse time may be slow, i.e. if there is bad internet connection or the server is slow.
+//Therefore, await makes sure we fetch all the data before moving on.
+//async: whaiting for a promise is an asynchronous operation. Therefore we have to use async in combination with await
 async function getData(dataSource) {
+  //Fetching means we are getting data from a server.
   const response = await fetch(dataSource);
   const data = await response.json();
   return data;
@@ -35,42 +47,51 @@ function addCharacters(character) {
     .addEventListener("click", characterClicked);
 
   function characterClicked() {
+    showCharacterModal(character);
     showModal();
 
-    document.querySelector("#dialog-name").textContent = character.name;
-    document.querySelector("#dialog-image").src = character.image;
-    document.querySelector("#dialog-nickname").textContent = character.nickname;
-    document.querySelector("#dialog-catchphrase").textContent =
-      character.catchPhrase;
-    document.querySelector("#dialog-occupation").textContent =
-      character.occupation;
-    document.querySelector("#dialog-age").textContent = character.age;
-    document.querySelector("#dialog-voicedby").textContent = character.voicedBy;
-    document.querySelector("#dialog-gender").textContent = character.gender;
-    document.querySelector("#dialog-religion").textContent = character.religion;
-    document.querySelector("#dialog-haircolor").textContent =
-      character.hairColor;
-    document.querySelector("#dialog-schoolgrade").textContent =
-      character.schoolGrade;
-    document.querySelector("#dialog-episodes").textContent = character.episodes;
-    document.querySelector("#dialog-appearances").textContent =
-      character.appearances;
-    document.querySelector("#dialog-first-apperance").textContent =
-      character.firstApperance;
+    function showCharacterModal(character) {
+      document.querySelector("#dialog-name").textContent = character.name;
+      document.querySelector("#dialog-image").src = character.image;
+      document.querySelector("#dialog-nickname").textContent =
+        character.nickname;
+      document.querySelector("#dialog-catchphrase").textContent =
+        character.catchPhrase;
+      document.querySelector("#dialog-occupation").textContent =
+        character.occupation;
+      document.querySelector("#dialog-age").textContent = character.age;
+      document.querySelector("#dialog-voicedby").textContent =
+        character.voicedBy;
+      document.querySelector("#dialog-gender").textContent = character.gender;
+      document.querySelector("#dialog-religion").textContent =
+        character.religion;
+      document.querySelector("#dialog-haircolor").textContent =
+        character.hairColor;
+      document.querySelector("#dialog-schoolgrade").textContent =
+        character.schoolGrade;
+      document.querySelector("#dialog-episodes").textContent =
+        character.episodes;
+      document.querySelector("#dialog-appearances").textContent =
+        character.appearances;
+      document.querySelector("#dialog-first-apperance").textContent =
+        character.firstApperance;
+    }
 
     tooYoungToDrink(character);
+
     document
       .querySelector("#dialog-btn-close")
       .addEventListener("click", closeModal);
-    document
-      .querySelector("#dialog-character")
-      .addEventListener("click", closeModal);
+    // document
+    //   .querySelector("#dialog-character")
+    //   .addEventListener("click", closeModal);
   }
 }
 //Shows modal
 function showModal() {
   document.querySelector("#dialog-character").showModal();
   document.querySelector(".overlay").classList.remove("hidden");
+  document.querySelector("#dialog-character").style.opacity = "0";
   setTimeout(() => {
     document.querySelector("#dialog-character").style.opacity = "1";
     document.querySelector(".overlay").style.opacity = "1";
@@ -83,10 +104,14 @@ function closeModal() {
   document.querySelector(".overlay").style.opacity = "0";
   setTimeout(() => {
     document.querySelector(".overlay").classList.add("hidden");
+    document.querySelector("#dialog-character").style.opacity = "0";
   }, 500);
 }
 
-function tooYoungToDrink(character) {
+const description = tooYoungToDrink(character);
+document.querySelector("#dialog-character").textContent = description;
+
+description = function tooYoungToDrink() {
   if (character.age <= 20) {
     document.querySelector(
       "#dialog-age"
@@ -103,17 +128,5 @@ function tooYoungToDrink(character) {
     ).textContent = `${character.name} is too old
     for shitface fridays`;
   }
-}
-
-// function showAllCharacters(list) {}
-// showAllCharacters(character);
-
-// function showCharacter(character) {}
-
-// function showDialog(character) {}
-
-// function closeDialog() {}
-
-// function getDescription() {}
-// function getDescription() {}
-// function formatDate() {}
+  return description;
+};
